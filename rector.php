b9e43d52f2c92fd054b9f51e161624b088d5c76a
Reza Moradi <sirmerdas@gmail.com>
2025-03-26 06:20:32 +0330

    feat: Add php rector package

---
+++ rector.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+    ])
+    ->withPreparedSets(codeQuality: true, deadCode: true, naming: true, typeDeclarations: true);
